cd786f256924abc0dde881a00287b5a2d02a0064
TwilightVoyager777 <TwilightVoyager777@gmail.com>
2026-03-03 09:20:00 +0800

    feat: create Xcode project with iOS (SnapClock) and watchOS (SnapClockWatch) targets

---
+++ SnapClock/SnapClock/SnapClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapClockApp.swift
+//  SnapClock
+//
+//  Created by 橡皮擦 on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct SnapClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnapClock/SnapClockWatch Watch App/SnapClockWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapClockWatchApp.swift
+//  SnapClockWatch Watch App
+//
+//  Created by 橡皮擦 on 3/30/26.
+//
+
+import SwiftUI
+
+@main
+struct SnapClockWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
