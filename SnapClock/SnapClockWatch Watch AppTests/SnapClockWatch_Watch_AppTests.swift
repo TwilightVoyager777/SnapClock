7b268713c10b54b5b15d5ee02dff5127fc51c9be
TwilightVoyager777 <TwilightVoyager777@gmail.com>
2026-03-30 14:51:50 -0700

    feat: create Xcode project with iOS (SnapClock) and watchOS (SnapClockWatch) targets

---
+++ SnapClock/SnapClockWatch Watch AppTests/SnapClockWatch_Watch_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnapClockWatch_Watch_AppTests.swift
+//  SnapClockWatch Watch AppTests
+//
+//  Created by 橡皮擦 on 3/30/26.
+//
+
+import Testing
+@testable import SnapClockWatch_Watch_App
+
+struct SnapClockWatch_Watch_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
